64bc39b5d790170b4ea1ff2f9ff7efe9914210f5
Jeremy <Jeremy@LAPTOP-NHDRHL0L>
2020-01-08 19:55:00 +0700

    Tambah ArrayList buat history convert (kalo jadi)

---
+++ ConverterOODA/src/com/unitconverter/java/Data.java
@@ -0,0 +1,12 @@
+package com.unitconverter.java;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Data {
+	static ArrayList<String> history1 = new ArrayList<>(Arrays.asList());
+	static ArrayList<String> history2 = new ArrayList<>(Arrays.asList()); 
+	static ArrayList<String> history3 = new ArrayList<>(Arrays.asList()); 
+	static ArrayList<String> history4 = new ArrayList<>(Arrays.asList()); 
+
+}
