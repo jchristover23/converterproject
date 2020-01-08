c285cfe1ac353c84cc64034ec4926fd582f4cbd6
Jeremy <Jeremy@LAPTOP-NHDRHL0L>
2020-01-08 22:49:14 +0700

    Added ArrayList to save past data

--- ConverterOODA/src/com/unitconverter/java/History.java
+++ ConverterOODA/src/com/unitconverter/java/History.java
@@ -3,9 +3,9 @@ package com.unitconverter.java;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class Data {
-	static ArrayList<String> history1 = new ArrayList<>(Arrays.asList());
-	static ArrayList<String> history2 = new ArrayList<>(Arrays.asList()); 
+public class History {
+	static ArrayList<String> tempHistory = new ArrayList<>(Arrays.asList());
+	static ArrayList<String> lengthHistory = new ArrayList<>(Arrays.asList()); 
 	static ArrayList<String> history3 = new ArrayList<>(Arrays.asList()); 
 	static ArrayList<String> history4 = new ArrayList<>(Arrays.asList());
 
