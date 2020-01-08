44b6eec9a100490399765ad849517948e6eb4451
Jeremy <Jeremy@LAPTOP-NHDRHL0L>
2020-01-08 20:01:43 +0700

    -

--- ConverterOODA/src/com/unitconverter/java/Data.java
+++ ConverterOODA/src/com/unitconverter/java/Data.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Data {
-	//Buat history convert kalo jadi
 	static ArrayList<String> history1 = new ArrayList<>(Arrays.asList());
 	static ArrayList<String> history2 = new ArrayList<>(Arrays.asList()); 
 	static ArrayList<String> history3 = new ArrayList<>(Arrays.asList()); 
