4dc1c059eaf3df637279bb0b75c38bcdf3f889b3
Jeremy <Jeremy@LAPTOP-NHDRHL0L>
2020-01-08 19:57:57 +0700

    Tambah arraylist

--- ConverterOODA/src/com/unitconverter/java/Data.java
+++ ConverterOODA/src/com/unitconverter/java/Data.java
@@ -4,9 +4,10 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Data {
+	//Buat history convert kalo jadi
 	static ArrayList<String> history1 = new ArrayList<>(Arrays.asList());
 	static ArrayList<String> history2 = new ArrayList<>(Arrays.asList()); 
 	static ArrayList<String> history3 = new ArrayList<>(Arrays.asList()); 
-	static ArrayList<String> history4 = new ArrayList<>(Arrays.asList()); 
+	static ArrayList<String> history4 = new ArrayList<>(Arrays.asList());
 
 }
